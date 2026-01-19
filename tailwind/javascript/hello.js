24dd8a8dee8465f13fa473cdfe7377ad4fe3f7b3
ciandre1976 <ciandre23@gmail.com>
2026-01-19 22:08:49 +0100

    feat: add object logging and age update in hello.js

--- tailwind/javascript/hello.js
+++ tailwind/javascript/hello.js
@@ -1,2 +1,12 @@
+"use strict";
+
 const sasa = "sasa";
 console.log(`hello ${sasa}`);
+let object = {
+  name: "sasa",
+  age: 18,
+};
+console.log(object.name);
+console.log(object.age);
+object.age = 20;
+console.log(object.age);
