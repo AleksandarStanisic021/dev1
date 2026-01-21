c63a3c183999b9f467c08caf905d4a9c3a19ac3b
ciandre1976 <ciandre23@gmail.com>
2026-01-21 03:32:31 +0100

    feat: add new examples for object-oriented programming and enhance Car and Person classes with additional methods

--- tailwind/javascript/oop.js
+++ tailwind/javascript/oop.js
@@ -1,4 +1,4 @@
-let compouter = {
+/*let compouter = {
   brand: "TechBrand",
   getBrand: function () {
     return this.brand;
@@ -55,3 +55,60 @@ console.log(dog.age); // Output: 15
 
 console.log(dog.speak()); // Output: Buddy makes a sound.
 console.log(Animal.prototype);
+*/
+console.log("OOP in JavaScript");
+
+let computer = {
+  cpu: "Intel i7",
+};
+let laptop = {
+  ram: "16GB",
+};
+Object.setPrototypeOf(laptop, computer);
+
+//console.log(laptop.cpu); // Accessing inherited property
+//console.log(laptop.__proto__); // Accessing own property
+
+let animal = {
+  legs: 4,
+};
+let dog = {
+  name: "Buddy",
+};
+Object.setPrototypeOf(dog, animal);
+//console.log(Object.getPrototypeOf(dog));
+//console.log(dog.legs);
+
+let ball = {
+  type: "rounded",
+};
+let soccerBall = {
+  brand: "Adidas",
+};
+Object.setPrototypeOf(soccerBall, ball);
+//console.log(soccerBall.type);
+//console.log(Object.getPrototypeOf(soccerBall));
+
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+Person.prototype.greet = function () {
+  return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+};
+let p = new Person("Alice", 30);
+//console.log(p.greet());
+
+function Car(make, model) {
+  this.make = make;
+  this.model = model;
+}
+Car.prototype.getDetails = function () {
+  return `${this.make} ${this.model}`;
+};
+Car.prototype.startEngine = function () {
+  return "Engine started";
+};
+let myCar = new Car("Toyota", "Corolla");
+console.log(myCar.getDetails());
+console.log(myCar.startEngine());
