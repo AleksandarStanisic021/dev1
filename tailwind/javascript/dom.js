12b526326cf228fc82234c9778dd959d92e57fe6
ciandre1976 <ciandre23@gmail.com>
2026-01-21 19:39:24 +0100

    feat: add index.html with basic structure and styles for DOM manipulation examples

--- tailwind/javascript/dom.js
+++ tailwind/javascript/dom.js
@@ -1,1 +1,31 @@
+let btn = document.getElementById("btn");
 
+btn.addEventListener("click", function () {
+  let p = document.getElementById("myPara");
+  p.textContent = "Button Clicked!";
+});
+
+let list = document.getElementById("list");
+let listButton = document.getElementById("listButton");
+listButton.addEventListener("click", function () {
+  let hold = document.getElementById("list");
+  hold.firstElementChild.classList.add("highlight");
+  // hold.lastElementChild.classList.add("highlight");
+  //hold.children[1].classList.add("highlight");
+});
+let tasks = document.getElementById("tasks");
+let taskButton = document.getElementById("taskButton");
+taskButton.addEventListener("click", function () {
+  let newTask = document.createElement("li");
+  newTask.textContent = "New Task Added";
+  tasks.appendChild(newTask);
+});
+
+let removeButton = document.getElementById("remove");
+
+removeButton.addEventListener("click", function () {
+  let tasks = document.getElementById("tasks");
+  if (tasks.lastElementChild) {
+    tasks.removeChild(tasks.lastElementChild);
+  }
+});
