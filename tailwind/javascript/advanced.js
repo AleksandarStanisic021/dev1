0dc7d5cb29561a015a9d02d1a6b0ab4626deff36
ciandre1976 <ciandre23@gmail.com>
2026-01-22 16:28:31 +0100

    fix1

---
+++ tailwind/javascript/advanced.js
@@ -0,0 +1,49 @@
+console.log("hi");
+
+/*const fetchdata = function () {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (true) resolve("data fetched");
+      else reject("error occurred");
+    }, 2000);
+  });
+};
+
+let data = fetchdata();
+
+data.then((res) => console.log(res)).catch((err) => console.log(err));
+
+async function fetchdataAsync() {
+  try {
+    let response = await fetchdata();
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+fetchdataAsync();*/
+/*
+function outer() {
+  let count = 0;
+  return function () {
+    count++;
+    return count;
+  };
+}
+let counter = outer();
+console.log(counter());*/
+
+function outer() {
+  let count = 0;
+  return function () {
+    count++;
+    return count;
+  };
+}
+
+let counter1 = outer();
+let counter2 = outer();
+console.log(counter1());
+console.log(counter1());
+console.log(counter1());
+console.log(counter2());
